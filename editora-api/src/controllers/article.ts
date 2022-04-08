8577506701d8e88505f0c1856093e41dd948d2cf
hironha <rafaelhirokato@gmail.com>
2022-04-08 17:59:01 -0300

    :bug: fix way to pass data to controller

--- editora-api/src/controllers/article.ts
+++ editora-api/src/controllers/article.ts
@@ -1,6 +1,10 @@
-import { CreateArticle, FindAllArticles, UpdateArticle } from "services/article";
+import {
+  CreateArticle,
+  FindAllArticles,
+  UpdateArticle,
+} from 'services/article';
 
-import type { Controller } from "types/controller";
+import type { Controller } from 'types/controller';
 
 export const findAll: Controller = async (req, res) => {
   const findAllArticles = new FindAllArticles();
@@ -11,26 +15,40 @@ export const findAll: Controller = async (req, res) => {
 };
 
 export const createArticle: Controller = async (req, res) => {
-  const create = new CreateArticle(req.body);
+  const create = new CreateArticle({
+    author: req.body.author,
+    published: req.body.published,
+    published_at: req.body.published,
+    summary: req.body.summary,
+    title: req.body.title,
+  });
 
   try {
     const createdArticle = await create.exec();
 
     res.status(200).json(createdArticle);
   } catch (err) {
-    res.status(500).json({ message: "Algo de inesperado aconteceu." });
+    res.status(500).json({ message: 'Algo de inesperado aconteceu.' });
   }
 };
 
 export const updateArticle: Controller = async (req, res) => {
   const { id } = req.params;
-  const update = new UpdateArticle({ id: Number(id), ...req.body });
+  const update = new UpdateArticle({
+    id: Number(id),
+    author: req.body.author,
+    is_active: req.body.is_active,
+    published: req.body.published,
+    published_at: req.body.published_at,
+    summary: req.body.published,
+    title: req.body.title,
+  });
 
   try {
     const updatedArticle = await update.exec();
 
     res.status(200).json(updatedArticle);
   } catch (err) {
-    res.status(500).json({ message: "Algo de inesperado aconteceu." });
+    res.status(500).json({ message: 'Algo de inesperado aconteceu.' });
   }
 };
